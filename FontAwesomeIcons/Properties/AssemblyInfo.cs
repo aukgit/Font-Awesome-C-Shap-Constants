1c938fcdcb17bdcf60556d60cf8f36f3e63657b8
Alim Ul Karim <devorg.bd@gmail.com>
2016-03-01 18:45:35 +0600

    initial push
    initial push

---
+++ FontAwesomeIcons/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FontAwesomeIcons")]
+[assembly: AssemblyDescription("Font Awesome Icons Constants for C#.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alim Ul Karim")]
+[assembly: AssemblyProduct("FontAwesomeIcons")]
+[assembly: AssemblyCopyright("Open source")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3490a69-2c53-4211-8694-8a15d8775cd5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
